277a92e78965e3175b903224337afc93c53a210d
Gourav2609 <herculeswarrior.in@gmail.com>
2024-03-14 17:13:12 +0530

    import Fix

--- routes/blog/blogs.js
+++ routes/blog/blogs.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Article = require('../../models/Article');
 const multer = require('multer');
+const cors = require('cors');
 const app = express();
 
 app.use(cors({
