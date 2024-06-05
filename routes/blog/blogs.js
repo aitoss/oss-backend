1d7b37a85279f39ceabe05681bb6ea43bebf6f3d
Binit <binitlenka@gmail.com>
2024-06-05 20:35:09 +0530

    Added: Database Migration

--- routes/blog/blogs.js
+++ routes/blog/blogs.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Article = require('../../models/Article');
+const mongoose = require('mongoose')
 const algoliasearch = require("algoliasearch")
 const { v4: uuid } = require('uuid');
 // const multer = require('multer');
@@ -39,6 +40,42 @@ router.get('/blogs', async (req, res) => {
   }
 });
 
+router.get('/startMigration', async (req, res) => {
+    try {
+        //mongoose.connect('mongodb://admin:password@localhost:27017/ANUBHAV-AITOSS', { useNewUrlParser: true, useUnifiedTopology: true });
+        const articleSchema = new mongoose.Schema({
+            id: String,
+            title: String,
+            typeOfArticle: String,
+            companyName: String,
+            description: String,
+            articleTags: [String],
+            isAuthentic: Boolean,
+            showName: Boolean,
+            author: {
+                name: String,
+                contact: String
+            },
+            imageUrl: String,
+            createdAt: Date
+        });
+
+        const Article = mongoose.model('articles', articleSchema);
+
+        const articles = await Article.find();
+
+        await index.saveObjects(articles.map(article => ({
+            title: article.title,
+            authorName: article.author.name,
+            objectID: article._id
+        })));
+        
+        console.log("Data Migration to algolia completed succesfully")
+    } catch (error) {
+        console.error('Error Migrating Data to algolia', error)
+    }
+});
+
 // @route  GET /api/anubhav/blog/:id
 // @desc   get a single blog by its ID
 // @access public
@@ -59,22 +96,15 @@ router.get('/blog/:index', async (req, res) => {
 // @desc   implement search and filters
 // @access public
 router.get('/search', async (req, res) => {
-  const query = req.query.q;
+    const query = req.query.q;
 
-  const baseQuery = {$text: {$search: query}};
+    console.log(query)
 
-  try {
-    const suggestions = await Article.find(baseQuery, {
-      score: {$meta: 'textScore'},
-    })
-        .sort({score: {$meta: 'textScore'}})
-
-    res.json(suggestions);
-  } catch (error) {
-    console.error('Error searching for suggestions:', error);
-    res.status(500).json({message: 'Internal server error'});
-  }
+    index.search(query).then(({hits}) => {
+        res.status(200).json({ results: hits })
+    });
 });
+
 
 // @route  GET /api/anubhav/search
 // @desc   implement search and filters
@@ -161,7 +191,20 @@ router.post('/blogs', async (req, res) => {
       imageUrl: image,
     });
 
-    await createArticle.save();
+    const savedarticle = await createArticle.save();
+    const articleId = savedArticle._id;
+
+    const objectForAlgolia = [{
+        title: title,
+        author: authorName,
+        objectID: articleId,
+    }]
+
+    const response = await index.saveObjects(objectForAlgolia, {autoGenerateObjectIDIfNotExist: true})
+    if(!response || response.error) {
+        console.error("Error Uploading objects to Algolia: ", response ? response.message: "Unknown Error");
+        return res.status(500).json({ message: 'Failed to Upload article to Algolia' });
+    }
     res
         .status(201)
         .json({message: 'Article created successfully', createArticle});
