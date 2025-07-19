7c99d2eb8c326322e83a0dc0e4965fa671358ac6
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2025-07-19 17:28:13 +0530

    update cors

--- constants.js
+++ constants.js
@@ -2,6 +2,7 @@ const adminEmail = process.env.ADMINMAIL;
 const adminPasswordHash = process.env.ADMINHASH;
 
 const allowedOrigins = [
+  "http://localhost:3000",
   "http://localhost:5173",
   "https://anubhav.aitoss.club"
 ];
