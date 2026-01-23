946c387d575f333151424d6149d77e75fddf01d8
Pradeep-kumar-py <pk4401128@gmail.com>
2026-01-23 13:34:12 +0530

    feat: add additional allowed origins for CORS

--- constants.js
+++ constants.js
@@ -4,7 +4,8 @@ const adminPasswordHash = process.env.ADMINHASH;
 const allowedOrigins = [
   "http://localhost:3001",
   "http://localhost:5173",
-  "https://anubhav.aitoss.club"
+  "https://anubhav.aitoss.club",
+  "https://anubhav.aitoss.in",
 ];
 
 module.exports = { adminEmail, adminPasswordHash, allowedOrigins }
